540f6c97aaef336270786024952d0f7c03188e55
markobrkovic <querry999@gmail.com>
2022-08-09 01:02:02 +0200

    ✨ Install redux packages and create simple redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import "./App.css";
 import WelcomePage from "./pages/welcomePage/WelcomePage";
 
 function App() {
   return (
-    <div className="App">
-      <WelcomePage />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <WelcomePage />
+      </div>
+    </Provider>
   );
 }
 
