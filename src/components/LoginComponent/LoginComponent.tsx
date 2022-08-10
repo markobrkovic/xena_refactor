58105bb123a37bff93f0e59a21efd109f862273c
markobrkovic <querry999@gmail.com>
2022-08-11 00:32:21 +0200

    ✨ Add missing redux package modules and connect login component

--- src/components/LoginComponent/LoginComponent.tsx
+++ src/components/LoginComponent/LoginComponent.tsx
@@ -1,15 +1,28 @@
 import ButtonElement from "../../elements/ButtonElement/ButtonElement";
 import InputElement from "../../elements/InputElement/InputElement";
+import { login } from "../../store/actions/loginActions";
+import store from "../../store/store";
+import { connect, ConnectedProps } from "react-redux";
 
-function LoginComponent() {
+function LoginComponent(props: PropsFromRedux) {
   return (
     <>
       <h2>Login</h2>
       <InputElement type="text" text="enter username" />
       <InputElement type="password" text="enter password" />
-      <ButtonElement text="login" onClick={() => console.log("mrs")} />
+      <ButtonElement text="login" onClick={props.login} />
     </>
   );
 }
 
-export default LoginComponent;
+const mapDispatchToProps = (dispatch: typeof store.dispatch) => {
+  return {
+    login: () => dispatch(login()),
+  };
+};
+
+const connector = connect(null, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+export default connector(LoginComponent);
